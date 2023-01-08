bcf3c549d0077e90625414f680a9a1fc202dca3f
Opix <opixgamer@gmail.com>
2023-01-08 18:06:53 +0100

    test uglyfy

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+
 
 const About = () => {
+
+	useEffect(() => {
+		document.title = "OD | About"
+	}, [])
 	return (
 		<div>
 			About
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+
+
 
 const Home = () => {
+
+	useEffect(() => {
+		document.title = "OD | Home"
+	}, [])
+
 	return (
 		<div>
 			Home
